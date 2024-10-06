7f1c0e55db0d2086ec4b4dcfb89d93342e293775
venkata trinadh <venkatatrinadh4444@gmail.com>
2024-10-06 16:50:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let image1=document.getElementById('image1');
+let image2=document.getElementById('image2');
+
+function spring() {
+    image1.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+
+    image2.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+    
+}
+function summer() {
+   image1.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+
+   image2.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+}
+function autumn() {
+    image1.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+
+   image2.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+}
+function winter() {
+    image1.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+
+    image2.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+    
+}
